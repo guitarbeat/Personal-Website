66dbdd82cd0988d5d3cf1bd5a7b10328825d38d1
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2022-12-19 15:07:05 -0600

    test

--- src/App.js
+++ src/App.js
@@ -98,21 +98,27 @@ function Header() {
       <div className="container__content">
         <div className="header">
           <a>
-            <img
-              src={process.env.PUBLIC_URL + "/profile1.png"}
-              className="header__picture"
-              alt="me"
-            />
-            <img
-              src={process.env.PUBLIC_URL + "/profile2.png"}
-              className="header__picture"
-              alt="me"
-            />
-            <img
-              src={process.env.PUBLIC_URL + "/profile3.png"}
-              className="header__picture"
-              alt="logo"
-              />
+          <img
+      src={process.env.PUBLIC_URL + "/profile1.png"}
+      class="header__picture"
+      alt="me"
+      onMouseOver={() => setHoveredImage(1)}
+      onMouseOut={() => setHoveredImage(0)}
+    />
+    <img
+      src={process.env.PUBLIC_URL + "/profile2.png"}
+      class="header__picture"
+      alt="me"
+      onMouseOver={() => setHoveredImage(2)}
+      onMouseOut={() => setHoveredImage(0)}
+    />
+    <img
+      src={process.env.PUBLIC_URL + "/profile3.png"}
+      class="header__picture"
+      alt="logo"
+      onMouseOver={() => setHoveredImage(3)}
+      onMouseOut={() => setHoveredImage(0)}
+    />
           </a>
           <div className="header__text">
             <h1>Aaron </h1>

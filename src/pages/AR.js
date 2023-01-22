cfb50282c892716467bd4fc69df87bec4e6ac60d
Aaron Lorenzo Woods <alwoods@utexas.edu>
2023-01-22 17:17:05 +0000

    Update 6 files

--- src/pages/AR.js
+++ src/pages/AR.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "@google/model-viewer/dist/model-viewer";
-import style from "../pages/style.css";
+import "../pages/styles.css";
 
 const AR = () => (
   <div id="card">
     <model-viewer
-      src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948"
-      ios-src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.usdz?v=1569545377878"
-      poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
-      alt="A 3D model of an astronaut"
-      shadow-intensity="1"
-      camera-controls
-      auto-rotate
+      src="https://foil-vasc.s3.us-east-2.amazonaws.com/blood.glb"
+      ios-src="https://foil-vasc.s3.us-east-2.amazonaws.com/blood.usdz"
       ar
-    ></model-viewer>
+      ar-modes="webxr scene-viewer quick-look"
+      camera-controls
+      poster="https://foil-vasc.s3.us-east-2.amazonaws.com/poster.webp"
+      shadow-intensity="1"
+    >
+      <div class="progress-bar hide" slot="progress-bar">
+        <div class="update-bar"></div>
+      </div>
+      <button slot="ar-button" id="ar-button">
+        View in your space
+      </button>
+      <div id="ar-prompt">
+        <img src="ar_hand_prompt.png"></img>
+      </div>
+    </model-viewer>
   </div>
 );
 

606dfc94deab2d26a4418c7c863176c3ec62b269
Aaron Lorenzo Woods <alwoods@utexas.edu>
2023-01-23 18:32:11 +0000

    Update 4 files

--- src/pages/ar.js
+++ src/pages/ar.js
@@ -2,44 +2,26 @@ import React from "react";
 import "@google/model-viewer";
 import styles from "../pages/styles.css";
 
-// const AR = () => (
-//   <div id="card">
-//     <model-viewer
-//       src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.glb?1542147958948"
-//       ios-src="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b/Astronaut.usdz?v=1569545377878"
-//       poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
-//       alt="A 3D model of an astronaut"
-//       shadow-intensity="1"
-//       camera-controls
-//       auto-rotate
-//       ar
-//     ></model-viewer>
-//   </div>
-// );
-// export default AR;
 const AR = () => (
   <div class="card-container">
     <div id="card">
       <model-viewer
         src="blood-v1.glb"
-        // ios-src="https://foil-vasc.s3.us-east-2.amazonaws.com/blood.usdz"
-        poster="poster.webp"
-        alt="A 3D model of vasculature"
+        ios-src="https://foil-vasc.s3.us-east-2.amazonaws.com/blood.usdz"
         ar
-        ar-modes="webxr scene-viewer quick-look"
+        ar-modes="scene-viewer webxr quick-look"
         camera-controls
+        poster="poster.webp"
         shadow-intensity="1"
+        auto-rotate
       >
-        <div
-          className={`${styles.progressBar} ${styles.hide}`}
-          slot="progress-bar"
-        >
-          <div className={styles.bar}></div>
+        <div class="progress-bar hide" slot="progress-bar">
+          <div class="update-bar"></div>
         </div>
-        <button className={styles.button} slot="ar-button">
+        <button slot="ar-button" id="ar-button">
           View in your space
         </button>
-        <div className={styles.ar - prompt}>
+        <div id="ar-prompt">
           <img src="ar_hand_prompt.png" alt="ar_hand_prompt" />
         </div>
       </model-viewer>

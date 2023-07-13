57013e82c36c9f5809aba3173e1f82026a66543f
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-07-13 18:45:20 +0000

    Update 22 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// File: App.js
-
 // Import styles
 import "./sass/main.scss";
 
 // Import dependencies
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import GoogleSheetsProvider from "react-db-google-sheets"; //https://github.com/athersharif/metis/tree/main
+import GoogleSheetsProvider from "react-db-google-sheets";
 
 // Import components
 import { NavBar, Header, About, Projects, Work } from "./components";
@@ -17,6 +15,7 @@ import ThemeSwitcher from "./themeSwitcher";
 const AR = lazy(() => import("./pages/ar"));
 const AR2 = lazy(() => import("./pages/ar2"));
 const Therosafe = lazy(() => import("./pages/therosafe"));
+const Friends = lazy(() => import("./pages/fun/friends"));
 
 // Custom loading component with CSS animation
 const CustomLoadingComponent = () => (
@@ -25,60 +24,86 @@ const CustomLoadingComponent = () => (
   </div>
 );
 
+// Configuration for main Google Sheet
 const config = {
-  apiKey: "AIzaSyBeKUeUWLmgbvcqggGItv9BPrQN1yyxRbE", // Replace with your actual API key
+  apiKey: "AIzaSyBeKUeUWLmgbvcqggGItv9BPrQN1yyxRbE",
   discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-  spreadsheetId: "1kYcFtsMQOap_52pKlTfWCYJk1O5DD66LlZ90TWCgAyA", // Replace with your actual Spreadsheet ID
+  spreadsheetId: "1kYcFtsMQOap_52pKlTfWCYJk1O5DD66LlZ90TWCgAyA",
   dataLoading: {
     component: CustomLoadingComponent,
   },
 };
 
+// NavBar items
+const navBarItems = {
+  About: "/#about",
+  Projects: "/#projects",
+  Work: "/#work",
+};
+
+const LayoutWithNavBar = ({ children }) => (
+  <>
+    <div className="vignete-top" />
+    <NavBar items={navBarItems} />
+    {children}
+    <div className="vignete-bottom" />
+    <div id="magicContainer">
+      <MagicComponent />
+    </div>
+  </>
+);
+
+const LayoutWithoutNavBar = ({ children }) => (
+  <>
+    <div className="vignete-top" />
+    {children}
+    <div className="vignete-bottom" />
+    <div id="magicContainer">
+      <MagicComponent />
+    </div>
+  </>
+);
+
+// Higher-order components
+const withLayout = (Component) => (props) =>
+  (
+    <LayoutWithNavBar>
+      <Component {...props} />
+    </LayoutWithNavBar>
+  );
+
+const withFriendsLayout = (Component) => (props) =>
+  (
+    <LayoutWithoutNavBar>
+      <Component {...props} />
+    </LayoutWithoutNavBar>
+  );
+
 const App = () => (
   <GoogleSheetsProvider config={config}>
     <ThemeSwitcher />
     <BrowserRouter>
-      <div>
-        <div className="vignete-top" />
-        <NavBar
-          items={{
-            About: "/#about",
-            Projects: "/#projects",
-            Work: "/#work",
-            // Models: "/ar",
-            // Therosafe: "/therosafe",
-          }}
-        />
-        <div>
-          <Switch>
-            <Suspense fallback={<div>Loading...</div>}>
-              {/* Home route */}
-              <Route
-                exact
-                path="/"
-                render={() => (
-                  <>
-                    <Header />
-                    <About />
-                    <Projects />
-                    <Work />
-                  </>
-                )}
-              />
-
-              <Route exact path="/ar" component={AR} />
-              <Route exact path="/ar2" component={AR2} />
-              <Route exact path="/therosafe" component={Therosafe} />
-              <Route path="*" render={() => <Redirect to="/" />} />
-            </Suspense>
-          </Switch>
-        </div>
-        <div />
-        <div id="magicContainer">
-          <MagicComponent />
-        </div>
-        <div className="vignete-bottom" />
-      </div>
+      <Switch>
+        <Suspense fallback={null}>
+          {/* Home route */}
+          <Route
+            exact
+            path="/"
+            component={withLayout(() => (
+              <>
+                <Header />
+                <About />
+                <Projects />
+                <Work />
+              </>
+            ))}
+          />
+          <Route exact path="/ar" component={withLayout(AR)} />
+          <Route exact path="/ar2" component={withLayout(AR2)} />
+          <Route exact path="/therosafe" component={withLayout(Therosafe)} />
+          <Route exact path="/friends" component={withFriendsLayout(Friends)} />
+        </Suspense>
+      </Switch>
     </BrowserRouter>
   </GoogleSheetsProvider>
 );

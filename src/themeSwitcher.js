8fc8db98f7b37a0fec68ef34da1d140add075c90
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-10-03 23:48:37 +0000

    Update themeSwitcher.js

--- src/themeSwitcher.js
+++ src/themeSwitcher.js
@@ -1,27 +1,31 @@
 import { useEffect } from "react";
 
 const ThemeSwitcher = () => {
-  useEffect(() => {
-    const updateTheme = () => {
-      const currentHour = new Date().getHours();
-      const body = document.body;
-      if (currentHour >= 6 && currentHour < 18) {
-        body.classList.add("light-theme");
-      } else {
-        body.classList.remove("light-theme");
-      }
-    };
-
-    const themeSwitch = document.querySelector(".theme-switch");
-    if (themeSwitch) {
-      themeSwitch.addEventListener("click", () => {
-        document.body.classList.toggle("light-theme");
-      });
+  // Update the theme based on the current time of day
+  const updateTheme = () => {
+    const currentHour = new Date().getHours();
+    const body = document.body;
+    if (currentHour >= 6 && currentHour < 18) {
+      body.classList.add("light-theme");
+    } else {
+      body.classList.remove("light-theme");
     }
+  };
 
+  // Add an event listener to the theme switch button
+  const themeSwitch = document.querySelector(".theme-switch");
+  if (themeSwitch) {
+    themeSwitch.addEventListener("click", () => {
+      document.body.classList.toggle("light-theme");
+    });
+  }
+
+  // Update the theme on mount and on resize
+  useEffect(() => {
     updateTheme();
     window.addEventListener("resize", updateTheme);
 
+    // Remove the event listener on unmount
     return () => {
       if (themeSwitch) {
         themeSwitch.removeEventListener("click", () => {
@@ -32,6 +36,7 @@ const ThemeSwitcher = () => {
     };
   }, []);
 
+  // Return null since this component is a side effect only
   return null;
 };
 

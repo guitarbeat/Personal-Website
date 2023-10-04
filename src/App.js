3615030bcf1e71ac245925d45ca392ebb4c931b9
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-10-04 00:43:53 +0000

    Update App.js and projects.scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-// Import styles
 import "./sass/main.scss";
-
-// Import dependencies
 import React, { Suspense, memo } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import GoogleSheetsProvider from "react-db-google-sheets";
-
-// Import components
 import { NavBar, Header, About, Projects, Work } from "./components";
 import MagicComponent from "./Magic";
 import ThemeSwitcher from "./themeSwitcher";
 
-// Custom loading component with CSS animation
 const CustomLoadingComponent = () => (
   <div id="magicContainer">
     <MagicComponent />
   </div>
 );
 
-// Configuration for main Google Sheet
 const config = {
   apiKey: "AIzaSyBeKUeUWLmgbvcqggGItv9BPrQN1yyxRbE",
   discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
@@ -28,35 +21,38 @@ const config = {
   },
 };
 
-// NavBar items
 const navBarItems = {
   About: "/#about",
   Projects: "/#projects",
   Work: "/#work",
 };
 
-// Layout component
-const Layout = memo(({ children, withNavBar }) => (
-  <React.Fragment>
+const Layout = memo(({ children, navItems }) => (
+  <>
     <div className="vignete-top" />
-    {withNavBar ? (
-      <NavBar items={navBarItems} />
-    ) : (
-      <NavBar items={{ Home: "/#header" }} />
-    )}
+    <NavBar items={navItems} />
     {children}
     <div className="vignete-bottom" />
     <div id="magicContainer">
       <MagicComponent />
     </div>
-  </React.Fragment>
+  </>
 ));
 
-// Higher-order component to wrap a component in the Layout component
-const withLayout = (Component) => (props) => (
-  <Layout withNavBar>
-    <Component {...props} />
-  </Layout>
+const withLayout = (Component, navItems) => (props) =>
+  (
+    <Layout navItems={navItems}>
+      <Component {...props} />
+    </Layout>
+  );
+
+const HomePageContent = () => (
+  <>
+    <Header />
+    <About />
+    <Projects />
+    <Work />
+  </>
 );
 
 const App = () => (
@@ -68,16 +64,9 @@ const App = () => (
           <Route
             exact
             path="/"
-            render={withLayout(() => (
-              <React.Fragment>
-                <Header />
-                <About />
-                <Projects />
-                <Work />
-              </React.Fragment>
-            ))}
+            render={withLayout(HomePageContent, navBarItems)}
           />
-          <Redirect to="/" />{" "}
+          <Redirect to="/" />
         </Switch>
       </Suspense>
     </BrowserRouter>

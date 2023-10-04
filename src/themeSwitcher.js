162dc1e5db6a7c4decc67ea0027dd35102d4594e
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-10-04 00:31:08 +0000

    Update 3 files

--- src/themeSwitcher.js
+++ src/themeSwitcher.js
@@ -1,42 +1,40 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const ThemeSwitcher = () => {
+  const [isLightTheme, setIsLightTheme] = useState(false);
+
   // Update the theme based on the current time of day
   const updateTheme = () => {
     const currentHour = new Date().getHours();
+    setIsLightTheme(currentHour >= 6 && currentHour < 18);
+  };
+
+  // Toggle the theme
+  const toggleTheme = () => {
+    setIsLightTheme((prevTheme) => !prevTheme);
+  };
+
+  useEffect(() => {
+    updateTheme();
+
+    const themeSwitch = document.querySelector(".theme-switch");
+    if (themeSwitch) {
+      themeSwitch.addEventListener("click", toggleTheme);
+
+      // Return a cleanup function
+      return () => themeSwitch.removeEventListener("click", toggleTheme);
+    }
+  }, []);
+
+  useEffect(() => {
     const body = document.body;
-    if (currentHour >= 6 && currentHour < 18) {
+    if (isLightTheme) {
       body.classList.add("light-theme");
     } else {
       body.classList.remove("light-theme");
     }
-  };
+  }, [isLightTheme]);
 
-  // Add an event listener to the theme switch button
-  const themeSwitch = document.querySelector(".theme-switch");
-  if (themeSwitch) {
-    themeSwitch.addEventListener("click", () => {
-      document.body.classList.toggle("light-theme");
-    });
-  }
-
-  // Update the theme on mount and on resize
-  useEffect(() => {
-    updateTheme();
-    window.addEventListener("resize", updateTheme);
-
-    // Remove the event listener on unmount
-    return () => {
-      if (themeSwitch) {
-        themeSwitch.removeEventListener("click", () => {
-          document.body.classList.toggle("light-theme");
-        });
-      }
-      window.removeEventListener("resize", updateTheme);
-    };
-  }, []);
-
-  // Return null since this component is a side effect only
   return null;
 };
 

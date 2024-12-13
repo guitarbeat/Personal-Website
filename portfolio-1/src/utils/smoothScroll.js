573c72e9a6e121b29165a6ab6a655369c2fd8475
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2024-12-13 13:51:55 -0600

    fixes

--- portfolio-1/src/utils/smoothScroll.js
+++ portfolio-1/src/utils/smoothScroll.js
@@ -28,8 +28,6 @@ if (!('scrollBehavior' in document.documentElement.style)) {
       if (t < 1) {
         return c / 2 * t * t + b;
       }
-        return c / 2 * t * t + b;
-      }
       t--;
       return -c / 2 * (t * (t - 2) - 1) + b;
     };

af5d46d36370d0c6ee63afffb5986ae240459e3c
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2024-12-14 00:31:05 -0600

    whatevs

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 export const GOOGLE_SHEETS_CONFIG = {
   apiKey: process.env.REACT_APP_GOOGLE_SHEETS_API_KEY,
   discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-  spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID,
+  spreadsheetId: process.env.REACT_APP_GOOGLE_SHEETS_DOC_ID,
   dataLoading: {
-    component: () => null // Moved CustomLoadingComponent to App.js since it depends on MagicComponent
+    component: () => null
   },
 };
 
